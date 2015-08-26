1290ae1ae691f5da5e2531bbc78889e814c1c361
jacob_schwartz <jacob@glint.io>
2015-08-25 22:35:06 -0700

    Initial Commit

---
+++ display/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  display
+//
+//  Created by glint on 8/25/15.
+//  Copyright (c) 2015 OMGPANDA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
